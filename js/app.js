673eed1363278324e3a9b2a21fbeab1955606ae5
Janine <schneiders.janine@gmail.com>
2017-08-29 18:03:59 -0300

    Layout and UX/UI

--- js/app.js
+++ js/app.js
@@ -1,38 +1,92 @@
-//set some agenda items for demonstration
-var agendaItems = {
-    0: {name: 'Admin',
-      files: []}, 
-    1: {name: 'Minutes Last Meeting',
-      files: []}, 
-    2: {name: 'Performance',
-      files: []}, 
-    3: {name: 'Management Update',
-      files: []},       
-};
-//put the items into the view
-var index = 0;
-for(var el in agendaItems){ 
-    index = parseInt(el, 10) + 1;
-    $('#agendaTab ul').append("<li><span>"+index+"</span><span>"+agendaItems[el].name+"</span></li>");
-}
-index += 1;
-$('#agendaTab ul').append("<li><span id='newItemIndex'>"+index+"</span><input class='addNewItem' id='addNewItem' placeholder='Enter a new agenda item...' /></li>");
-
-$form = $('#form');
-var droppedFiles = false;
-//listen for the drag file
-$form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
-    e.preventDefault();
-    e.stopPropagation();    
-})
-.on('drop', function(e) {
-    droppedFiles = e.originalEvent.dataTransfer.files;
-    console.log(droppedFiles);
-});
-
 $(document).ready(function(){
+    //set some agenda items for demonstration
+    var agendaItems = {
+        0: {name: 'Admin',
+        files: []}, 
+        1: {name: 'Minutes Last Meeting',
+        files: []}, 
+        2: {name: 'Performance',
+        files: []}, 
+        3: {name: 'Management Update',
+        files: []},       
+    };
+    //put the items into the view
+    var index = 0;
+    for(var el in agendaItems){ 
+        index = parseInt(el, 10) + 1;
+        $('#agendaTab ul').append("<li><span>"+index+"</span><span>"+agendaItems[el].name+"</span></li>");
+        $('#agendaItemSelect').append("<option value='"+index+"'>"+agendaItems[el].name+"</option>");
+    }
+    index += 1;
+    $('#agendaTab ul').append("<li><span id='newItemIndex'>"+index+"</span><input class='addNewItem' id='addNewItem' placeholder='Enter a new agenda item...' /></li>");
+
+
+    $form = $('#form');
+    var droppedFiles = false;
+    //listen for the drag file
+    $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+        e.preventDefault();
+        e.stopPropagation();    
+    })
+    .on('drop', function(e) {
+        droppedFiles = e.originalEvent.dataTransfer.files;
+    });
+
     $('#browseFromComputer').click(function(){
         $('#file').trigger('click');
+    });
+    var filesCounter = 0;
+    $('#addFile').click(function(){
+        if(droppedFiles){
+            itemToAddFile = $('#agendaItemSelect').val() - 1;
+            for(var i = 0; i < droppedFiles.length; i++ ){
+                $('#showFiles').append('<p>'+droppedFiles[i].name+'</p>');
+                agendaItems[itemToAddFile].files.push(droppedFiles[i]);
+                filesCounter += 1;
+            }            
+            droppedFiles = false;
+            if($('#file').val()){
+                fileName = $('#file').val().split('\\');
+                 $('#showFiles').append('<p>'+fileName[2]+'</p>');
+                agendaItems[itemToAddFile].files.push(fileName[2]);
+                filesCounter += 1;
+                $('#file').val('');
+            }
+        }else{
+            if($('#file').val()){
+                itemToAddFile = $('#agendaItemSelect').val() - 1;
+                fileName = $('#file').val().split('\\');
+                 $('#showFiles').append('<p>'+fileName[2]+'</p>');
+                agendaItems[itemToAddFile].files.push(fileName[2]);
+                filesCounter += 1;
+                $('#file').val('');
+            }
+        }
+
+        
+        $('#filesCounter').html(filesCounter);
+    });
+
+    $('#closeFileCard').click(function(){
+        $('.fileCard').animate({
+            bottom: "64"
+        },300, function(){
+            $(this).hide();
+        });
+        $('.insideFileCard').animate({
+            top: "29"
+        },300, function(){
+            $(this).hide();
+        });
+        isFileCardOpen = false;
+    });
+
+    $('#publishButton').click(function(){
+        $('#publishCard').fadeIn('slow');
+    });
+
+    $('#closePublishCard').click(function(){
+        $('#publishCard').hide();
     });
 
     //control the card for files
@@ -41,12 +95,12 @@ $(document).ready(function(){
         if(isFileCardOpen){
             $('.fileCard').animate({
                 bottom: "64"
-            },400, function(){
+            },300, function(){
                 $(this).hide();
             });
             $('.insideFileCard').animate({
                 top: "29"
-            },400, function(){
+            },300, function(){
                 $(this).hide();
             });
             isFileCardOpen = false;
@@ -67,9 +121,13 @@ $(document).ready(function(){
             //get global index
             var name = $(this).val();
             lastLi = $(this).parent();
+            name = name.charAt(0).toUpperCase() + name.slice(1);
             $("<li><span>"+index+"</span><span>"+name+"</span></li>").insertBefore(lastLi);
+            $('#agendaItemSelect').append("<option value='"+index+"'>"+name+"</option>");
+            agendaItems[index] = {name: name,  files: []};
             index += 1;
             $('#newItemIndex').text(index);
+            $(this).val('Enter a new agenda item...');
         }
     });
 });
